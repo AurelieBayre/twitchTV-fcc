8788c25eebb5ba78421be2eed4ad4cfe367e5b87
Aureliemacspi <a.bayre@gmail.com>
2017-08-28 18:20:29 +0200

    building the layout

--- tpjs.js
+++ tpjs.js
@@ -1,107 +1,17 @@
-$(document).ready(function() {
-	var userNames = [
-		"ESL_SC2",
-		"OgamingSC2",
-		"cretetion",
-		"freecodecamp",
-		"storbeck",
-		"habathcx",
-		"RobotCaleb",
-		"noobs2ninjas"
-	];
-	var fccName = "";
-	var fccBio = "";
-	var fccUrl = "";
-	var name2 = "";
-	var bio2 = "";
+$(document).ready(function()
+{
+	$("#on").click(function()
+	{
+		$("#results").html("<p>This will show the twitchers who are currently streaming.</p>");
+	})
 
-	//about freecodecamp
+	$("#off").click(function()
+	{
+		$("#results").html("<p>This will show the twitchers who are offline.</p>");
+	})
 
-	$.ajax({
-		type: "GET",
-		dataType: "jsonp", //next time don't forget the Cap on Type, it's not datatype but dataType!!!
-		url: "https://wind-bow.gomix.me/twitch-api/users/freecodecamp?callback=?",
-		success: function(data) {
-			fccName = data.display_name;
-			fccBio = data.bio;
-		}
-	});
-
-	$.ajax({
-		type: "GET",
-		dataType: "jsonp", //next time don't forget the Cap on Type, it's not datatype but dataType!!!
-		url: "https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?",
-		success: function(plusData) {
-			fccUrl = plusData.url;
-		}
-	});
-
-	$.ajax({
-		type: "GET",
-		dataType: "jsonp", //next time don't forget the Cap on Type, it's not datatype but dataType!!!
-		url: "https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?",
-		success: function(streaming) {
-			var stream = streaming.stream;
-			if (stream === null) {
-				$("#fcc").html(
-					"<h2>" +
-						fccName +
-						"</h2><h3><a href='" +
-						fccUrl +
-						"' target='_blank'> is currently offline... </a>:'(</h3> <p>" +
-						fccBio +
-						"</p>"
-				);
-			} else {
-				$("#fcc").html(
-					"<h2>" +
-						fccName +
-						"</h2><h3><a href='" +
-						fccUrl +
-						"' target='_blank'> is currently online! </a> :) </h3> <p>" +
-						fccBio +
-						"</p>"
-				);
-			}
-		}
-	});
-
-	//about the others
-
-	for (var x = 0; x < userNames.length; x++) {
-		$.ajax({
-			type: "GET",
-			dataType: "jsonp",
-			url:
-				"https://wind-bow.gomix.me/twitch-api/users/" +
-					userNames[x] +
-					"?callback=?",
-			success: function(data2) {
-				name2 = data2.display_name;
-				bio2 = data2.bio;
-			} //end of success
-		}); //end of ajax data2
-
-		$.ajax({
-			type: "GET",
-			dataType: "jsonp",
-			url:
-				"https://wind-bow.gomix.me/twitch-api/streams/" +
-					userNames[x] +
-					"?callback=?",
-			success: function(data3) {
-				if (data3.stream === null) {
-					$("#offline").prepend(
-						$("<div/>", { id: "offUsers" + x, class: "offUsersInfo" })
-					);
-					$("#offUsers" + x).html("<h3>" + name2 + "</h3> <p>" + bio2 + "</p>");
-				} else {
-					$("#online").prepend(
-						$("<div/>", { id: "onUsers" + x, class: "onUsersInfo" })
-					);
-					$("onUsers" + x).html("<h3>" + name2 + "</h3> <p>" + bio2 + "</p>");
-				}
-			}
-		});
-	} //end of for
-}); //end of doc
+	$("#all").click(function()
+	{
+		$("#results").html("<p>All the channels will be displayed here.</p>");
+	})
+})
